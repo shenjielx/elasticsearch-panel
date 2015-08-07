b0b9f0727a56f490220fcf8cc68627ed1a30bbe0
沈大侠 <shenjielx@gmail.com>
2015-08-07 18:57:25 +0800

    move to _site
    move to _site all files

--- _site/js/basic.js
+++ _site/js/basic.js
@@ -1,14 +1,20 @@
 var statistics = function () {
+    var fileds_list = [];
     var url=$(".J_connectUrl").val();//"http://112.124.51.187:9200/";
-    if (location.href.indexOf("http://")>0) {
-      url=location.href;
+
+    var get_url = function(){
+      url = $(".J_connectUrl").val();
+      if (location.href.indexOf("http://")>0) {
+        url=location.href;
+      }
+      if (url.length<1) {
+        url="http://localhost:9200/";
+      }
       if(url.indexOf("/_plugin/")>0) {
-        var base_uri = url.replace(/_plugin\/.*/, '');
+        url = url.replace(/_plugin\/.*/, '');
       }
     }
-    if (url.length<1) {
-      url="http://localhost:9200/";
-    }
+
     //url=location.href;
 
     // 获取ElasticSearh基本节点信息
@@ -47,7 +53,7 @@ var statistics = function () {
           error: function (res) {
             $(".J_connectWarning").text("连接失败！");
             layer.close(index);
-            layer.msg('连接失败！',{shade: [0.8, '#393D49']});
+            layer.msg('连接失败！',{time:1000,shade: [0.8, '#393D49']});
           }
       });
 
@@ -112,8 +118,7 @@ var statistics = function () {
             }
         });
     }
-
-
+    // 获取文档列表
     var set_docs_list=function(docs){
       var _htm='';
       $.each(docs[0], function(key, val) {  // key是序号:1,2,...., val是遍历值.
@@ -121,24 +126,26 @@ var statistics = function () {
         _htm += '<option value="'+key+'">'+key+'('+temp+'个文档)</option>';
       });
       $('.searchDocs').html(_htm);
-    }
 
+      get_docs_mapping();
+    }
+    // 初始化设置
     var handelControls = function () {
-        // 收缩和展开字段列
-        $(".J_flexHide").click(function(){
-  				$(this).hide();
-  				$(".J_flex").hide();
-  				$(".J_flexShow").show();
-  			});
-  			$(".J_flexShow").click(function(){
-  				$(this).hide();
-  				$(".J_flex").show();
-  				$(".J_flexHide").show();
-  			});
+        $(".J_connectUrl").val(location.href);
 
         //连接ES集群
         $(".btnConnect").click(function(){
           get_collapse_node_info();
+        });
+
+        // 获取文档mapping映射字段
+        $(".searchDocs").change(function(){
+          get_docs_mapping();
+        });
+
+        // 获取字段赋值控件
+        $(".searchField").change(function(){
+          set_field_operation(this);
         });
 
         //添加新的条件行
@@ -147,7 +154,7 @@ var statistics = function () {
         });
         set_default();
     }
-
+    // 设置部分初始化操作
     var set_default=function(){
         //删除条件行
         $(".btnDel").click(function(){
@@ -159,13 +166,37 @@ var statistics = function () {
         });
     }
 
+    // 设置字段赋值输入框的初始化
+    var set_init_operation = function(){
+      // 获取字段赋值输入框
+      $(".searchOperation").change(function(){
+        $(this).parent("span").next().remove();
+        var val = $(this).val();
+        var _input = set_field_input(val);
+        $(this).parent("span").after(_input);
+        $(".J_chkFields")[0].checked = true;
+      });
+    }
+
+    // 设置查询的字段
+    var set_default_fields = function(){
+      $(".J_chkFields").click(function(){
+        var isChecked = $(this)[0].checked;
+        var list = $('#collapse_field .panel-body input');
+        $.each(list,function(key,val){
+          $(this)[0].checked = isChecked;
+        });
+      })
+    }
+
     //添加条件行
     var add_column=function(obj){
-      var btnHtml=$(obj).parent("span").html();
+      //var btnHtml=$(obj).parent("span").html();
       var parent=$(obj).parents("dd");
-      var col=parent.children(".search-column:first");
-      var _htm=col.html();
-      _htm=_htm.replace(btnHtml,"");
+      var col1=parent.find(".search-column span:first");
+      var col2=parent.find(".search-column span:last");
+      var _htm='<span>'+col1.html()+'<span>'+'<span>'+col2.html()+'<span>';
+      //_htm=_htm.replace(btnHtml,"");
       $('<div/>', {
         'class': 'search-column',
         'style':'margin-bottom:10px;',
@@ -174,14 +205,100 @@ var statistics = function () {
       set_default();
     }
 
+    // 获取文档mapping映射
+    var get_docs_mapping=function(){
+      $("#search_time").text('--');
+      $("#search_total").text('--');
+      $(".J_result").html('');
+      var index = layer.load(2, {time: 30*1000,shade: [0.8, '#393D49']});
+      var sel_docs=$(".searchDocs").val();
+      var postData={};
+      $.ajax({
+          type: "GET",
+          data: postData,
+          url: url+"_cluster/state",
+          success: function (data) {
+            var mapping=parseJsonToField(data["metadata"]["indices"][sel_docs]);
+            $(".searchField").html(mapping);
+            $(".sortField").html(mapping);
+            layer.close(index);
+          },
+          error: function (res) {
+            layer.close(index);
+            layer.msg('获取文档mapping映射失败！',{shade: [0.8, '#393D49']});
+          }
+      });
+    }
+
+    // 解析json
+    var parseJsonToField = function collapse(data) {
+      var options=['<option value="match_all">match_all</option>'];
+      if (data) {
+        var mapping=data["mappings"];
+        var _chk_htm = '';// 设置该文档的所有字段
+        $.each(mapping, function(key_map, val_map) {
+          var index=key_map;
+          var fields=val_map["properties"];
+          $.each(fields, function(key_field, val_field) {
+            fileds_list.push(key_field);
+            var filed=index+"."+key_field;
+            var data_type=val_field["type"];
+            options.push('<option value="'+filed+'" data-type="'+data_type+'">'+filed+'</option>');
+            _chk_htm += '<span class="col-md-2 col-xs-3 col-sm-4"><input type="checkbox" class="chkShowJson" checked="true" value="'+ key_field +'" /><label class="form-label">'+ key_field +'</label></span>';
+          });
+        });
+        $("#collapse_field .panel-body").html(_chk_htm);
+        set_default_fields();
+      }
+
+      return options.join(" ");
+    }
+
+    // 设置字段赋值控件操作选项
+    var set_field_operation=function(obj){
+      var that=$(obj);
+      var val=that.val();
+      $(".searchOperation").parent("span").remove();
+      if (val.indexOf(".")>0) {
+        var _htm='<span class="span-Operation"><select class="searchOperation form-control pull-left" name="searchOperation" style="max-width:150px;">';
+        _htm+='<option value="term">term</option>';
+        _htm+='<option value="range">range</option>';
+        _htm+='<option value="fuzzy">fuzzy</option>';
+        _htm+='<option value="query_string">query_string</option>';
+        _htm+='<option value="missing">missing</option></select>';
+        _htm+='</select><span>';
+
+        that.parent("span").after(_htm);
+        var span_Operation=that.parents(".search-column").find(".span-Operation");
+        if (span_Operation!="undefined") {
+          var _input = set_field_input(span_Operation.find(".searchOperation").val());
+          span_Operation.after(_input);
+        }
+        set_init_operation();
+      }
+    }
+
+    // 设置字段赋值控件框
+    var set_field_input=function(val){
+      var _htm='<span>';
+      switch (val) {
+        case "term":
+        case "query_string":
+          _htm='<input type="text" class="searchInput form-control pull-left" name="searchInput" style="max-width:150px;" />';
+          break;
+        default:
+
+      }
+      _htm+='<span>';
+      return _htm;
+    }
+
     // last return
     return {
         init: function () {
             handelControls();
+            get_url();
             get_collapse_node_info();
-
-            //window.getuserlogs = getuserlogs;
-        }//,
-        //getuserlogs: getuserlogs
+        }
     };
 }();
---
+++ _site/js/search.js
@@ -0,0 +1,245 @@
+var search = function () {
+    var url=$(".J_connectUrl").val();//"http://112.124.51.187:9200/";
+    var pageIndex = 0;
+    var pageSize = 10;
+    var get_url = function(){
+      url = $(".J_connectUrl").val();
+      if (location.href.indexOf("http://")>0) {
+        url=location.href;
+      }
+      if (url.length<1) {
+        url="http://localhost:9200/";
+      }
+      if(url.indexOf("/_plugin/")>0) {
+        url = url.replace(/_plugin\/.*/, '');
+      }
+    }
+    //url=location.href;
+
+    var set_post_data = function (fields) {
+      pageSize = $(".pageSize").val();
+      var postData={
+        query:{
+          bool:{
+            must:[
+              {
+                match_all:{}
+              }
+            ],
+            must_not:[],
+            should:[]
+          }
+        },
+        from:pageIndex,
+        size:pageSize,
+        sort:[],
+        facets:{}
+      };
+      // 返回字段
+      if (fields != null) {
+        postData["_source"] = fields;
+      }
+      // 排序
+      var sortField = $(".sortField").val();
+    	if (sortField != "match_all" && sortField != "_all" && sortField.length>1) {
+    		var sortType = $(".sortType").val();
+    		var sortinfo = {};
+    		sortinfo = {sortField:{ "order": sortType }};
+    		postData.sort=sortinfo;
+    	}
+      var result = postData;
+      result = JSON.stringify(postData);
+      return result;
+    }
+
+    var get_chk_val = function(){
+      var fields = [];
+      var list = $('#collapse_field .panel-body input:checked');
+      $.each(list,function(key,val){
+        fields.push($(this).val());
+      });
+      return fields;
+    }
+
+    // 获取ElasticSearh基本节点信息
+    var search = function () {
+      var fields = get_chk_val();
+      var index = layer.load(2, {time: 30*1000,shade: [0.8, '#393D49']});
+      var sel_docs = $(".searchDocs").val();
+      var postData = set_post_data(fields);
+      $.ajax({
+          type: "POST",
+          data: postData,
+          dataType : "json",
+          url: url+sel_docs+"/_search",
+          success: function (data) {
+            var hits = data["hits"];
+
+            // 获取table格式
+            var _htm = '';
+            _htm = get_table(hits["hits"],fields);
+            $(".J_result").html(_htm);
+
+            // 查询耗时
+            var time = parseFloat(data["took"])/1000;
+            $("#search_time").text(time);
+
+            // 查询结果总数
+            var total = hits["total"];
+            $("#search_total").text(total);
+            set_pager(total);
+
+            // 收缩和展开字段列
+            $(".J_flexHide").click(function(){
+      				$(this).hide();
+      				$(".J_flex").hide();
+      				$(".J_flexShow").show();
+      			});
+      			$(".J_flexShow").click(function(){
+      				$(this).hide();
+      				$(".J_flex").show();
+      				$(".J_flexHide").show();
+      			});
+
+            layer.close(index);
+          },
+          error: function (res) {
+            layer.close(index);
+            layer.msg('查询失败！',{time:1000,shade: [0.8, '#393D49']});
+          }
+      });
+
+    }
+
+    // 设置分页
+    var set_pager = function(total){
+      var _htm = '';
+      var total_page = Math.ceil(total / pageSize);
+      if (pageIndex > 0) {
+        _htm += '<li><a class="J_pager" href="javascript:;" data-index="0" aria-label="first"><span aria-hidden="true">首页</span></a></li>';
+        _htm += '<li><a class="J_pager" href="javascript:;" data-index="0" aria-label="Previous"><span aria-hidden="true">上一页</span></a></li>';
+      }
+      var pager = [];
+      if (total_page > 1) {
+        var start = pageIndex > 5 ? (pageIndex - 2) : 0;
+        for (var i = start; i < start + 5; i++) {
+          var active = i == pageIndex ? ' class="active"' : '';
+          var pager_class = i != pageIndex ? ' class="J_pager"' : '';
+          _htm += '<li'+ active +'><a'+ pager_class +' href="javascript:;" data-index="'+ i +'">'+ (i + 1) +'</a></li>';
+        }
+      }
+      if (pageIndex <= total_page && total_page > 1) {
+        _htm += '<li><a class="J_pager" href="javascript:;" data-index="'+ total_page +'" aria-label="Next"><span aria-hidden="last">下一页</span></a></li>';
+          _htm += '<li><a class="J_pager" href="javascript:;" data-index="'+ total_page +'" aria-label="first"><span aria-hidden="true">尾页</span></a></li>';
+      }
+      $(".pagination").html(_htm);
+      set_pager_init();
+    }
+
+    // 返回Table
+    var get_table = function(list,fields){
+      var _htm = '<table class="table table-bordered table-hover">';
+      var _thead = '<thead><tr>';
+      _thead += '<th style="min-width:60px;">序号 <a class="glyphicon glyphicon-triangle-right pull-right J_flexShow" aria-hidden="true" href="javascript:;" style="display:none;"></a></th>';
+      _thead += '<th class="J_flex">_index</th>';
+      _thead += '<th class="J_flex">_type</th>';
+      _thead += '<th class="J_flex">_id</th>';
+      _thead += '<th class="J_flex" style="min-width: 120px;">_score <a class="pull-right J_flexHide" href="javascript:;"><span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span> 收缩</a></th>';
+
+      for (var i = 0; i < fields.length; i++) {
+        var field = fields[i];
+        _thead += '<th>'+ field +'</th>';
+      }
+      _thead += '</tr></thead>';
+
+      var _tbody = '<tbody>';
+      for (var i = 0; i < list.length; i++) {
+        _tbody += '<tr>';
+        _tbody += '<th scope="row">'+ (pageIndex * pageSize + i + 1) +'</th>';
+        var item = list[i];
+        _tbody += '<td class="J_flex" style="min-width:'+ (strlen(item["_index"]) * 10) +'px;">'+ item["_index"] +'</td>';
+        _tbody += '<td class="J_flex" style="min-width:'+ (strlen(item["_type"]) * 10) +'px;">'+ item["_type"] +'</td>';
+        _tbody += '<td class="J_flex" style="min-width:'+ (strlen(item["_id"]) * 10) +'px;">'+ item["_id"] +'</td>';
+        _tbody += '<td class="J_flex" style="min-width:'+ (strlen(item["_score"]) * 10) +'px;">'+ item["_score"] +'</td>';
+        var filed_val = item["_source"];
+
+        for (var j = 0; j < fields.length; j++) {
+          var field = fields[j];
+          var len = '';
+          var val = '';
+          if (field in filed_val) {
+            val = filed_val[field];
+            val = val!=null&&typeof(filed_val[field]) === 'object'?"--":val;
+            if (val != null) {
+              var strlength = strlen(val);
+              var len = ' style="min-width:'+ (strlength * 9) +'px;"';
+            }
+          }
+          _tbody += '<td'+ len +'>'+ HTMLDecode(val).replace(/\s+/g,"").replace("<","&lt;").replace(">","&gt;") +'</td>';
+        }
+        _tbody += '</tr>';
+      }
+      _tbody += '</tbody>';
+      _htm += _thead + _tbody;
+      _htm += '</table>';
+      return _htm;
+    }
+
+    // 计算字符串长度(英文占1个字符，中文汉字占2个字符)
+    function strlen(str){
+      var len = 0;
+      for (var i=0; i<str.length; i++) {
+       var c = str.charCodeAt(i);
+      //单字节加1
+       if ((c >= 0x0001 && c <= 0x007e) || (0xff60<=c && c<=0xff9f)) {
+         len++;
+       }
+       else {
+        len+=2.2;
+       }
+      }
+      return len;
+  }
+  var HTMLEncode = function(html)
+  {
+      var temp = document.createElement ("div");
+      (temp.textContent != null) ? (temp.textContent = html) : (temp.innerText = html);
+      var output = temp.innerHTML;
+      temp = null;
+      return output;
+  }
+
+  var HTMLDecode = function(text)
+  {
+      var temp = document.createElement("div");
+      temp.innerHTML = text;
+      var output = temp.innerText || temp.textContent;
+      temp = null;
+      return output;
+  }
+    // 初始化设置
+    var handelControls = function () {
+      // 搜索查询
+      $("#btnSearch").click(function(){
+        search();
+      });
+    }
+
+    // 分页初始化
+    var set_pager_init = function(){
+      $(".J_pager").click(function(){
+        pageIndex = $(this).data("index");
+        search();
+      })
+    }
+
+    // last return
+    return {
+        init: function () {
+            handelControls();
+            get_url();
+            //search();
+
+        }
+    };
+}();
